0a352ddeca730b523960a0c2ee5458e177857830
kynth65 <kynth65@gmail.com>
2025-11-11 20:46:59 +0800

    Add form helpers to password confirmation route
    Add form definition helpers to password confirmation route to support
    native HTML form submissions with proper HTTP methods.

--- resources/js/routes/password/confirm/index.ts
+++ resources/js/routes/password/confirm/index.ts
@@ -1,4 +1,4 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Auth\ConfirmablePasswordController::store
  * @see app/Http/Controllers/Auth/ConfirmablePasswordController.php:26
@@ -32,6 +32,28 @@ store.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
     url: store.url(options),
     method: 'post',
 })
+
+    /**
+* @see \App\Http\Controllers\Auth\ConfirmablePasswordController::store
+ * @see app/Http/Controllers/Auth/ConfirmablePasswordController.php:26
+ * @route '/confirm-password'
+ */
+    const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+        action: store.url(options),
+        method: 'post',
+    })
+
+            /**
+* @see \App\Http\Controllers\Auth\ConfirmablePasswordController::store
+ * @see app/Http/Controllers/Auth/ConfirmablePasswordController.php:26
+ * @route '/confirm-password'
+ */
+        storeForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+            action: store.url(options),
+            method: 'post',
+        })
+    
+    store.form = storeForm
 const confirm = {
     store: Object.assign(store, store),
 }
