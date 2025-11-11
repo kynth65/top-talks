351617f4f2ebbd79e698807abab2dbdaef70c1a6
kynth65 <kynth65@gmail.com>
2025-11-11 21:27:04 +0800

    Add new page routes
    Add routes for start-learning, teach-english, pricing, free-trial pages and trial submission API endpoint.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,24 @@ Route::get('/student-services', function () {
     return Inertia::render('StudentServices');
 })->name('student-services');
 
+Route::get('/start-learning', function () {
+    return Inertia::render('StartLearning');
+})->name('start-learning');
+
+Route::get('/teach-english', function () {
+    return Inertia::render('TeachEnglish');
+})->name('teach-english');
+
+Route::get('/pricing', function () {
+    return Inertia::render('ViewPricing');
+})->name('pricing');
+
+Route::get('/free-trial', function () {
+    return Inertia::render('StartFreeTrial');
+})->name('free-trial');
+
+Route::post('/api/trial/submit', [App\Http\Controllers\TrialController::class, 'store'])->name('trial.submit');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
